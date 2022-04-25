4ed95848da88ac5bcd1f27315a592a86683b538f
Dagi07 <56720713+Dagi07@users.noreply.github.com>
2022-04-26 00:21:01 +0300

    Update 0-memset.c

--- 0x06-pointers_arrays_strings/0-memset.c
+++ 0x06-pointers_arrays_strings/0-memset.c
@@ -1,9 +1,4 @@
-/*
- * File: 0-memset.c
- * Auth: Brennan D Baraban
- */
-
-#include "holberton.h"
+#include "main.h"
 
 /**
  * memset - Fills the first n bytes of the memory area
@@ -11,16 +6,15 @@
  * @s: A pointer to the memory area to be filled.
  * @c: The character to fill the memory area with.
  * @n: The number of bytes to be filled.
- *
  * Return: A pointer to the filled memory area @s.
  */
 void *_memset(void *s, int c, size_t n)
 {
-	unsigned int index;
-	unsigned char *memory = s, value = c;
+  unsigned int index;
+  unsigned char *memory = s, value = c;
 
-	for (index = 0; index < n; index++)
-		memory[index] = value;
+  for (index = 0; index < n; index++)
+    memory[index] = value;
 
 	return (memory);
 }
